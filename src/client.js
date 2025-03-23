94ff2f49b199d9330c75f0969d19d2545cc5edc5
Karnankita04 <91003709+Karnankita04@users.noreply.github.com>
2025-03-23 11:23:31 +0530

    ADD function working and tested

--- src/client.js
+++ src/client.js
@@ -1,16 +1,34 @@
 import { assertEquals } from "jsr:@std/assert";
 
-const request = "abc";
+const fetchResponse = async (connection) => {
+  const buffer = new Uint8Array(1024);
+  const byteRead = await connection.read(buffer);
+  const decoder = new TextDecoder();
+  const decodedData = decoder.decode(buffer.slice(0, byteRead));
+  return JSON.parse(decodedData);
+};
+
+const request = { command: "ADD", args: [1, 2] };
+
 const sendRequest = async (connection) => {
   const encoder = new TextEncoder();
-  const encodedData = encoder.encode(request);
+  const encodedData = encoder.encode(JSON.stringify(request));
   await connection.write(encodedData);
+};
+
+const test = async (connection) => {
+  await sendRequest(connection);
+  const actual = await fetchResponse(connection);
+  console.log(actual);
+  
+  assertEquals(actual, 3);
+  console.log("All test passed!!");
 };
 
 const main = async (port) => {
   const portNum = parseInt(port[0]);
   const connection = await Deno.connect({ port: portNum });
-  sendRequest(connection);
+  test(connection);
 };
 
 main(Deno.args);
--- src/server.js
+++ src/server.js
@@ -1,12 +1,34 @@
+const fetchRequest = (buffer) => {
+  const decoder = new TextDecoder();
+  const decodedData = decoder.decode(buffer);
+  const request = JSON.parse(decodedData);
+  return request;
+};
+
+const calculate = (command, a, b) => {
+  const commands = {
+    ADD: (a, b) => a + b,
+  };
+
+  return commands[command](a, b);
+};
+
+const sendResponse = async (command, args, conn) => {
+  console.log(command, args);
+  const encoder = new TextEncoder();
+  const result = calculate(command, ...args);
+  const encodedData = encoder.encode(JSON.stringify(result));
+  await conn.write(encodedData);
+};
+
 async function main(port) {
   const portNum = parseInt(port[0]);
   const listener = Deno.listen({ port: portNum });
 
   for await (const conn of listener) {
     for await (const buffer of conn.readable) {
-      const decoder = new TextDecoder();
-      const decodedData = decoder.decode(buffer);
-      console.log("Request: ", decodedData);
+      const { command, args } = fetchRequest(buffer);
+      sendResponse(command, args, conn);
     }
   }
 }
